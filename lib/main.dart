2214ffccf8e80a74128424084102f588eab839f3
Ibraguim Albakov <ibraguimalbakov@macbook-air-de-ibraguim.home>
2024-01-31 23:29:41 +0100

    1st_commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:ecommerce_app/pages/intro_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Ecommerce App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Intro_page(),
+    );
+  }
+}
---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,58 @@
+import 'dart:math';
+
+import 'package:ecommerce_app/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+class Intro_page extends StatelessWidget {
+  const Intro_page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double largeurEcran = MediaQuery.of(context).size.width;
+    double hauteurEcran = MediaQuery.of(context).size.height;
+    return Scaffold(
+        body: Center(
+      child: Column(
+        children: [
+          Padding(padding: EdgeInsets.all(hauteurEcran / 6)),
+          Image.asset("lib/images/adidas.png"),
+          const SizedBox(
+            height: 100,
+          ),
+          const Text(
+            "Welcome to Adidas",
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text(
+            "Buy your favourite shoes with us",
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              minimumSize: const Size(300, 50),
+              primary: Colors.black,
+              shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20))),
+            ),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => HomePage()),
+              );
+            },
+            child: const Text(
+              "Shop Now",
+              style: const TextStyle(fontSize: 20),
+            ),
+          )
+        ],
+      ),
+    ));
+  }
+}
